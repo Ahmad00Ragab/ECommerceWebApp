b9da2ebc026466237d87289b8ccdeb9f90c4a757
Antony-org <antonysameh1999@gmail.com>
2024-08-23 15:09:49 +0300

    dummy

--- src/main/java/gov/iti/jets/GenericDao/GenericDAO.java
+++ src/main/java/gov/iti/jets/GenericDao/GenericDAO.java
@@ -4,11 +4,8 @@ import java.util.Set;
 
 public interface GenericDAO<T> {
     T save(T entity);
+
     Set<T> findAll();
-
-    T findById(T id);
-
-    void delete(T id);
 
     T findById(java.lang.Integer id);
 
