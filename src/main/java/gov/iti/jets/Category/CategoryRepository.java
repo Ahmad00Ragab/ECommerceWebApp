7bca64f2cff7c4790c9d29c795c5371c313dd964
Antony-org <antonysameh1999@gmail.com>
2024-08-24 20:00:24 +0300

    get Products by Category

--- src/main/java/gov/iti/jets/Category/CategoryRepository.java
+++ src/main/java/gov/iti/jets/Category/CategoryRepository.java
@@ -1,12 +1,28 @@
 package gov.iti.jets.Category;
 
 import gov.iti.jets.GenericDao.GenericDaoImpl;
+import gov.iti.jets.Product.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+
+import java.util.List;
 
 public class CategoryRepository extends GenericDaoImpl {
 
     public CategoryRepository(Class entityClass, EntityManager entityManager, EntityTransaction transaction) {
         super(entityClass);
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Category> query = cb.createQuery(Category.class);
+        query.from(Category.class);
+
+        query.where(cb.equal(query.get("name"), category));
+
+
+        return em.createQuery(query).getResultList();
     }
 }
