73e7114498c5b5f27fac34ac75eeb81eb0cfa14d
AbdulMajeed Mohamed <abdulmajeed.ghndy9@outlook.com>
2024-09-05 21:35:19 +0300

    CartKey conflict solved

--- src/main/java/gov/iti/jets/cart/CartKey.java
+++ src/main/java/gov/iti/jets/cart/CartKey.java
@@ -2,36 +2,13 @@ package gov.iti.jets.cart;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-<<<<<<< HEAD
-import lombok.Getter;
-import lombok.Setter;
-=======
 import lombok.*;
->>>>>>> master
 
 import java.io.Serializable;
 
 @Embeddable
 @Setter
 @Getter
-<<<<<<< HEAD
-class CartKey implements Serializable {
-  
-    @Column(name = "user_id", nullable = false)
-    Long userId;
-  
-    @Column(name = "product_id", nullable = false)
-    Long productId;
-  
-    // standard constructors, getters, and setters
-    CartKey(Long userId, Long productId ) {
-        this.userId = userId;
-        this.productId = productId;
-    }
-
-    public CartKey() {}
-    // hashcode and equals implementation
-=======
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
@@ -43,6 +20,4 @@ class CartKey implements Serializable {
     @Column(name = "product_id", nullable = false)
     Long productId;
 
-
->>>>>>> master
 }
