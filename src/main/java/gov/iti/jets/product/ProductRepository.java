d44fdd86e042acffaea68e9b1307da43d301a818
AbdulMajeed Mohamed <abdulmajeed.ghndy9@outlook.com>
2024-09-06 20:56:58 +0300

    Updated the Product Repository and ProductService classes and add some methods like sorting by price and etc..

--- src/main/java/gov/iti/jets/product/ProductRepository.java
+++ src/main/java/gov/iti/jets/product/ProductRepository.java
@@ -2,14 +2,25 @@ package gov.iti.jets.product;
 
 import gov.iti.jets.genericDao.GenericDaoImpl;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class ProductRepository extends GenericDaoImpl<Product> {
+public class ProductRepository extends GenericDaoImpl<Product>{
+    private  EntityManager em;
+    public ProductRepository(EntityManager em) {
+        super(Product.class);
+        this.em = em;
+    }
 
     public ProductRepository(Class<Product> entityClass) {
         super(entityClass);
@@ -28,4 +39,40 @@ public class ProductRepository extends GenericDaoImpl<Product> {
             return Optional.of(em.createQuery(q).getSingleResult());
         }
     }
+
+
+
+    public Set<Product> findProductsByCategory(String category) {
+        String fetchingByCategory = "SELECT p FROM Product p WHERE p.category = :category";
+        Query query = em.createQuery(fetchingByCategory);
+        query.setParameter("category", category);
+        return new HashSet<>(query.getResultList());
+    }
+
+
+    public Set<Product> findProductsByPrice(BigDecimal minPrice, BigDecimal maxPrice) {
+        String fetchingByPrice="SELECT p FROM Product p WHERE p.price BETWEEN :minPrice AND :maxPrice";
+        Query query = em.createQuery(fetchingByPrice);
+        query.setParameter("minPrice", minPrice);
+        query.setParameter("maxPrice", maxPrice);
+        return new HashSet<>(query.getResultList());
+    }
+
+
+
+    public Set<Product> sortProductsByPrice() {
+        String sortByPriceQuery= "SELECT p FROM Product p ORDER BY p.price ASC";
+        Query query = em.createQuery(sortByPriceQuery);
+        return new HashSet<>(query.getResultList());
+
+    }
+
+
+    public Set<Product> sortProductsByCategoryAndPrice(String category) {
+        String sortProductCat = "SELECT p FROM Product p WHERE p.category.name = :category ORDER BY p.price ASC";
+        TypedQuery<Product> query = em.createQuery(sortProductCat, Product.class);
+        query.setParameter("category", category);
+        return new HashSet<>(query.getResultList());
+    }
+
 }
--- src/main/java/gov/iti/jets/product/ProductService.java
+++ src/main/java/gov/iti/jets/product/ProductService.java
@@ -1,5 +1,50 @@
 package gov.iti.jets.product;
 
+
+import gov.iti.jets.system.exceptions.ProductNotFoundException;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+
 public class ProductService {
-    
+
+    private final ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getProductByName(String name) {
+        Optional<Product> optProduct=productRepository.getProductByName(name);
+        Product product=null;
+        if(optProduct.isPresent()){
+            product=optProduct.get();
+        }else {
+            throw  new ProductNotFoundException();
+        }
+        return product;
+    }
+
+
+
+    public Set<Product> findProductsByCategory(String category) {
+        return  productRepository.findProductsByCategory(category);
+    }
+
+
+    public Set<Product> findProductsByPrice(BigDecimal minPrice, BigDecimal maxPrice) {
+        return productRepository.findProductsByPrice(minPrice, maxPrice);
+    }
+
+
+
+    public Set<Product> sortProductsByPrice() {
+        return productRepository.sortProductsByPrice();
+    }
+
+
+    public Set<Product> sortProductsByCategoryAndPrice(String category) {
+        return productRepository.sortProductsByCategoryAndPrice(category);
+    }
+
 }
