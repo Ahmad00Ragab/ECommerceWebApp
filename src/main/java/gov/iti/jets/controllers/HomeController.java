b8ea567e6cb6140c48a5e90560394870a1b3adea
Ghandy <abdulmajeed.ghndy@outlook.com>
2024-09-09 18:46:37 +0300

    updated register process

---
+++ src/main/java/gov/iti/jets/controllers/HomeController.java
@@ -0,0 +1,36 @@
+package gov.iti.jets.controllers;
+
+import gov.iti.jets.dtos.ProductDto;
+import gov.iti.jets.services.ProductService;
+import gov.iti.jets.services.UserService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet(name = "homeServlet", urlPatterns ={"/home", "/products"} )
+public class HomeController extends HttpServlet {
+    private ProductService productService = new ProductService();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          handleProducts(req,resp);
+    }
+
+
+
+    private  void handleProducts(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Set<ProductDto> homeProducts=productService.findAllProductsUsingDTO();
+        req.setAttribute("homeProducts", homeProducts);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/home.jsp");
+        dispatcher.forward(req,resp);
+    }
+
+
+
+
+}
